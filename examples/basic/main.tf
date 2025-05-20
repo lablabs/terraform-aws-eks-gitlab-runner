8595c057addf4a24df61c713e3c25f9df3467af7
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-05-20 23:19:15 +0200

    feat: universal-addon template migration

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -22,6 +22,23 @@ module "addon_installation_helm" {
   })
 }
 
+module "addon_installation_helm_pod_identity" {
+  source = "../../"
+
+  enabled           = true
+  argo_enabled      = false
+  argo_helm_enabled = false
+
+  cluster_name = module.eks_cluster.eks_cluster_id
+
+  irsa_role_create         = false
+  pod_identity_role_create = true
+
+  values = yamlencode({
+    # insert sample values here
+  })
+}
+
 # Please, see README.md and Argo Kubernetes deployment method for implications of using Kubernetes installation method
 module "addon_installation_argo_kubernetes" {
   source = "../../"
@@ -38,11 +55,10 @@ module "addon_installation_argo_kubernetes" {
   })
 
   argo_sync_policy = {
-    "automated" : {}
-    "syncOptions" = ["CreateNamespace=true"]
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
   }
 }
-
 
 module "addon_installation_argo_helm" {
   source = "../../"
@@ -54,8 +70,12 @@ module "addon_installation_argo_helm" {
   cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 
+  values = yamlencode({
+    # insert sample values here
+  })
+
   argo_sync_policy = {
-    "automated" : {}
-    "syncOptions" = ["CreateNamespace=true"]
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
   }
 }
