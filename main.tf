5bc63ca74da17a6db232b4c4335e35f274ed77ba
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-06-04 09:23:06 +0200

    fix: correct pre-commit workflow badge URL in README.md and main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
  * A Terraform module to deploy the [GitLab Runner](https://docs.gitlab.com/runner/) on Amazon EKS cluster.
  *
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/validate.yaml)
- * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/pre-commit.yml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-gitlab-runner/actions/workflows/pre-commit.yaml)
  */
 locals {
   addon = {
