660537550833c8d5496464e3ba605cb3bdd109b3
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2026-01-15 14:21:42 +0100

    feat(addon)!: add support for addon depends on

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
 # IMPORTANT: Add addon specific variables here
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
-  nullable    = false
-}
-
 variable "gitlab_url" {
   type        = string
   default     = "https://gitlab.com/"
